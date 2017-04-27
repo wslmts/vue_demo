3abf16fdc16fef69211274272c8cb5c6dc872bf8
cuicui.zhu <cuicui.zhu@dajie-inc.com>
2017-04-27 16:52:30 +0800

    add log

---
+++ shopping/js/address.js
@@ -0,0 +1,23 @@
+new Vue({
+    el:'#wrap',
+    data:{
+        title:'settle',
+        list:[],
+        showCount:3
+    },
+    mounted:function(){
+        this.getList();
+    },
+    computed:{
+       limitedList:function(){
+           return this.list.slice(0,this.showCount)
+       }
+    },
+    methods:{
+        getList:function(){
+            this.$http.get('./json/address.json').then(function(r){
+                this.list=r.body.list;
+            });
+        }
+    }
+});
